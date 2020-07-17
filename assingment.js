ecdaf4bbe28ff63688e285cb33b8c099de16ce7f
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-07-17 21:25:35 +0600

    almost done

--- assingment.js
+++ assingment.js
@@ -1,47 +1,77 @@
-// feetToMile
+// // feetToMile
 
 
 
 
-function feetToMile(feet){
-    var mile = feet * 0.00189394;
-    return mile;
-}
+// function feetToMile(feet){
+//     var mile = feet * 0.00189394;
+//     return mile;
+// }
 
-var result = feetToMile(3);
-console.log(result);
-
-
+// var result = feetToMile(3);
+// console.log(result);
 
 
-// woodCalculator
 
-function woodCalculator(chair, table,bed){
-    var chairCount = 1 * chair;
-    var tableCount = 3 * table;
-    var bedCount = 5 * bed;
-    var sum = chairCount + tableCount+ bedCount;
-    return sum;
-}
-var result = woodCalculator( 3 , 2 , 5);
-console.log(result);
+
+// // woodCalculator
+
+// function woodCalculator(chair, table,bed){
+//     var chairCount = 1 * chair;
+//     var tableCount = 3 * table;
+//     var bedCount = 5 * bed;
+//     var sum = chairCount + tableCount+ bedCount;
+//     return sum;
+// }
+// var result = woodCalculator( 3 , 2 , 5);
+// console.log(result);
 
 
 // brickCalculator
 
+function brickCalculator(floor){
+    var totalHeight;
+    var totalBrick;
+    if(floor<=10){
+        totalHeight = floor * 15;
+        totalBrick = totalHeight * 1000;
+    }   
+    else if (floor <=20 ){
+        totalHeight = (10 * 15) + (10 *12);
+        totalBrick = totalHeight * 1000;
+    }
+    else{
+       if ( floor >=21){
+        totalHeight = (10 *15)+(10 * 12)+(floor * 10);
+        totalBrick = totalHeight * 1000;
+
+       }
+        
+    }
+    return totalBrick;
+}
+var result = brickCalculator(21);
+console.log(result);
+
+
 
 // tinyFriend
 
-function  tinyFriend(n){
-    var tiny = n[0];
-    for (var i = 0; i < n.length; i++){
-        var element = n[i];
-        if (element < tiny){
-            tiny = element;
-        }
-    }
-    return tiny;
+// function  tinyFriend(n){
+//     var tiny = n[0];
+//     for (var i = 0; i < n.length; i++){
+//         var element = n[i];
+//         if (element < tiny){
+//             tiny = element;
+//         }
+//     }
+//     return tiny;
    
-}
-var result = tinyFriend(["shihab", "shibu" , "simul" , "sukkur"]);
-console.log(result);
+// }
+// var result = tinyFriend(["shihab", "shibu" , "simul" , "sukkur"]);
+// console.log(result);
+
+
+
+
+
